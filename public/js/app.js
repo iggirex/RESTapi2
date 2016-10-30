706b9b22d082b0a4a16f52836286d9757ca4c43f
Iggi <iggirex@gmail.com>
2016-10-30 17:28:08 -0600

    finishing project with angular front

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,26 @@
 angular.module("contacsApp", ['ngRoute'])
   .config(function($routeProvider) {
     $routeProvider
-      
+      .when("/", {
+        templateUrl: "list.html",
+        controller: "ListController",
+        resolve: {
+          contacts: function(Contacts) {
+            return Contacts.getContacts
+          }
+        }
+      })
   })
+  .service("Contacts", function($http) {
+    this.getContacts = function() {
+      return $http.get("/contacts")
+        .then(function(response) {
+          return response;
+        }, function(response) {
+          alert("Error retrieving contacts.");
+        });
+    }
+  })
+  .controller("ListController", function(contacts, $scope) {
+    $scope.contacts = contacts.data
+  });
